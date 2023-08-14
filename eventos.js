9138c9e803a07951719587d1b91d3002a2339fef
Martín Pereira <veridicotp22@gmail.com>
2023-08-13 23:18:11 -0300

    Solucion click ok

--- eventos.js
+++ eventos.js
@@ -2,7 +2,17 @@
 //En eventos.js agrega un manejador de eventos para el "click" en el div utilizando el método addEventListener. Debes programar que al dar click en el div salga una alerta con el texto "Hola! Soy el div".
 document.addEventListener('DOMContentLoaded', function () {//En eventos.js agrega un manejador de eventos para el "click" en el div utilizando el método addEventListener. Debes programar que al dar click en el div salga una alerta con el texto "Hola! Soy el div".
     const contenedor = document.getElementById('eldiv');
+    const boton = document.getElementById('boton');
+    let reg = "";
+    boton.addEventListener('click', function(){
+        reg="1";
+    });
     contenedor.addEventListener('click', function () {
-        alert('Hola! Soy el div');
+        if (reg==="1"){
+             alert("Hola!");
+             reg="";
+        }else {
+            alert('Hola! Soy el div');
+        }
     })
 })
